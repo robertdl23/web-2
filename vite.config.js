3729acb1f352d0b811fc5d681d80d5671864d5bd
Roberth De Leon <robertdl@umes.edu.gt>
2025-10-22 01:32:00 -0600

    build(vite): generar manifest en public/build durante docker build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
-import '../css/app.css'
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    manifest: true,          // genera public/build/manifest.json
-    outDir: 'public/build',
-    emptyOutDir: true,
-  },
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        cors: true,
+    },
+});
