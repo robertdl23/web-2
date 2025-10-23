87c136437fdbe1471416b262284e5af1cd462d40
Roberth De Leon <robertdl@umes.edu.gt>
2025-10-22 19:51:35 -0600

    Quito @vite del partial head y uso Tailwind CDN + app.css

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,14 +1,19 @@
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-<title>{{ $title ?? config('app.name') }}</title>
-
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+{{-- resources/views/partials/head.blade.php --}}
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
+{{-- Fuentes --}}
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@fluxAppearance
+{{-- Tailwind CDN (sin Vite) --}}
+<script>
+  window.tailwind = {
+    theme: { extend: { fontFamily: { sans: ['Inter','ui-sans-serif','system-ui'] } } }
+  }
+</script>
+<script src="https://cdn.tailwindcss.com"></script>
+
+{{-- Tu CSS plano para overrides (public/css/app.css) --}}
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+@stack('head')
